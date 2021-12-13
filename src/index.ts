9845d0e67def3e3a74a78aaf2f90ce1d79a2f194
NathanFelipe99 <nathanfguerlando@gmail.com>
2021-12-12 22:34:46 -0300

    configurando typeORM

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import routes from '../routes/routes'
+import 'reflect-metadata'
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3000, () => {
+    console.log("Server started at http://localhost:3000")
+})
