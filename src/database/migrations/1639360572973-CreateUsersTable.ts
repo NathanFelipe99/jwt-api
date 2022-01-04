38d7fc16fdffce2ee91f7d89fd49761534085597
NathanFelipe99 <nathanfguerlando@gmail.com>
2022-01-03 22:03:05 -0300

    commit

--- src/database/migrations/1639360572973-CreateUsersTable.ts
+++ src/database/migrations/1639360572973-CreateUsersTable.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class CreateUsersTable1639360572973 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
 
         await queryRunner.createTable(new Table({
             name: 'users',
